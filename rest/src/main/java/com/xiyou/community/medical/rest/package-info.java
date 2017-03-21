f857da5f4f5c30db8415be7819cdb8015d8d3bee
wxz <wxz@wxzdeMacBook-Pro.local>
2017-03-21 16:37:01 +0800

    update project dictionary

---
+++ rest/src/main/java/com/xiyou/community/medical/rest/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @Description:
+ * @author wangxinzhi.
+ * @date: 2017/3/8
+ * @time: 下午2:14
+ * @Copyright: Hangzhou Enniu Tech Ltd. All rights reserved.
+ */
+package com.xiyou.community.medical.rest;
---
+++ rest/src/main/java/com/xiyou/community/medical/rest/service/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @Description:
+ * @author wangxinzhi.
+ * @date: 2017/3/13
+ * @time: 上午11:56
+ * @Copyright: Hangzhou Enniu Tech Ltd. All rights reserved.
+ */
+package com.xiyou.community.medical.rest.service;
