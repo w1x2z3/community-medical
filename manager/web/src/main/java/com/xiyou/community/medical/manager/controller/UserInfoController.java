e51a05dc5a588656a1d96893b300bb2c0d1340c0
xiaoxindegithub <1563847040@qq.com>
2017-04-05 22:56:59 +0800

    update

--- manager/web/src/main/java/com/xiyou/community/medical/manager/controller/UserInfoController.java
+++ manager/web/src/main/java/com/xiyou/community/medical/manager/controller/UserInfoController.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
 @Controller
 @RequestMapping("/index")
 public class UserInfoController {
-    private Logger logger = Logger
+
 
     @Autowired
     private UserInfoService userInfoService;
